1e996491279c672a11ddddc205aeeb3d8f386b73
Official Account <officialaccount@Pralhads-MacBook-Pro.local>
2024-05-18 09:46:22 +0545

    first commit

---
+++ src/components/Contact/contact.js
@@ -0,0 +1,60 @@
+import React, { useRef } from 'react'
+import './contact.css'
+import NextNepal from '../../assets/walmart.png'
+import YopoSuperApp from '../../assets/microsoft.png'
+import FacebookIcon from '../../assets/facebook-icon.png'
+import InstagramIcon from '../../assets/instagram.png'
+import TwitterIcon from '../../assets/twitter.png'
+import LinkedInIcon from '../../assets/youtube.png'
+import emailjs from '@emailjs/browser';
+
+const Contact = () => {
+    const form = useRef();
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm('service_wpiujnw', 'template_s2mtzrc', form.current, {publicKey: 'SK8EOIRvrWgZFfgNd'})
+          .then(
+            () => {
+              console.log('SUCCESS!');
+              alert('Email sent !');
+              e.target.reset();
+            },
+            (error) => {
+              console.log('FAILED...', error.text);
+            },
+          );
+      };
+  return (
+    <section id='contactPage'>
+        <div id='clients'>
+            <h1 className='contactPageTitle'>My Experiences</h1>
+            <p className='clientDesc'>
+                I have had the opportunity to work with some companies, which includes
+            </p>
+            <div className='clientImgs'>
+                <img src={NextNepal} alt='' className='clientImg'/>
+                <img src={YopoSuperApp} alt='' className='clientImg'/>
+            </div>
+        </div>
+        <div id='contact'>
+            <h1 className='contactPageTitle'>Contact Me</h1>
+            <span className='contactDesc'>Please fill out the form below to discuss any work opportunities.</span>
+            <form className='contactForm' ref={form} onSubmit={sendEmail}>
+                <input type='text' className='name' placeholder='Your Name' name='your_name'/>
+                <input type='email' className='email' placeholder='Your Email' name='your_email'/>
+                <textarea className='msg' name='message' rows='5' placeholder='Write your message here...'></textarea>
+                <button type='submit' value='Send' className='submitBtn'>Submit</button>
+                <div className='links'>
+                    <img src={FacebookIcon} alt='Facebook' className='link'></img>
+                    <img src={InstagramIcon} alt='Instagram' className='link'></img>
+                    <img src={TwitterIcon} alt='Twitter' className='link'></img>
+                    <img src={LinkedInIcon} alt='LinkedIn' className='link'></img>
+                </div>
+            </form>
+        </div>
+    </section>
+  )
+}
+
+export default Contact
